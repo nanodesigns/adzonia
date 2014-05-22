5fc51cac62bd278ee1b254cb94911349b3409c3f
Mayeenul Islam <wz.islam@gmail.com>
2014-05-23 00:27:31 +0600

    Dev stage - not a stable version

--- adzonia-insert-edit.php
+++ adzonia-insert-edit.php
@@ -12,9 +12,6 @@ if( !isset( $_GET["action"] ) ) {
         $ad_code = $_POST['nano_ad_code'];
         $ad_image_url = $_POST['ad_image'];
         $ad_url = $_POST['nano_ad_url'];
-        $adsense_pub_id = $_POST['nano_adsense_pub_id'];
-        $adsense_ad_id = $_POST['nano_adsense_ad_id'];
-        $adsense_size = $_POST['nano_adsense_size'];
         $ad_str_time = $_POST['nano_ad_str_date'];
         $ad_end_time = $_POST['nano_ad_end_date'];
         $ad_status = ( isset( $_POST['nano_ad_active'] ) ? '1' : '0' );
@@ -26,9 +23,6 @@ if( !isset( $_GET["action"] ) ) {
             'ad_code'           => $ad_code,
             'ad_image_url'      => $ad_image_url,
             'url'               => $ad_url,
-            'adsense_pub_id'    => $adsense_pub_id,
-            'adsense_ad_slot'   => $adsense_ad_id,
-            'adsense_ad_size'   => $adsense_size,
             'str_time'          => $ad_str_time,
             'end_time'          => $ad_end_time,
             'ad_status'         => $ad_status
@@ -43,9 +37,6 @@ if( !isset( $_GET["action"] ) ) {
                     '%s',
                     '%s',
                     '%s',
-                    '%s',
-                    '%s',
-                    '%s',
                     '%d'    //integer
                     );
 
@@ -53,7 +44,6 @@ if( !isset( $_GET["action"] ) ) {
         if( ( ( $ad_str_time && $ad_end_time ) != '' )
             && (
                    ($_POST['nano_ad_type'] == 'imagead' && ($ad_image_url && $ad_url) != '')
-                || ($_POST['nano_ad_type'] == 'googleadsense' && ($adsense_pub_id && $adsense_ad_id && $adsense_size) != '')
                 || ($_POST['nano_ad_type'] == 'codead' && $ad_code != '')
             ) )
                 {
@@ -61,7 +51,7 @@ if( !isset( $_GET["action"] ) ) {
                     $success = $wpdb->insert( $table, $data, $format );
 
                     // Redirect to view ad page
-                    header( "location:" . admin_url('/admin.php?page=site-ad&addnew=1&success=true') );
+                    header( "location:" . admin_url('/admin.php?page=adzonia&addnew=1&success') );
                 }
 
         if(isset($success)) {
@@ -78,7 +68,7 @@ if( !isset( $_GET["action"] ) ) {
 * If the form is in Edit Mode
 */
 
-if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
+if( isset( $_GET["action"] ) && $_GET["action"] === 'edit' ) {
 
     global $wpdb;
     $table = $wpdb->wp_adzonia = $wpdb->prefix . "wp_adzonia";
@@ -93,9 +83,6 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
         $ad_code = $_POST['nano_ad_code'];
         $ad_image_url = $_POST['ad_image'];
         $ad_url = $_POST['nano_ad_url'];
-        $adsense_pub_id = $_POST['nano_adsense_pub_id'];
-        $adsense_ad_id = $_POST['nano_adsense_ad_id'];
-        $adsense_size = $_POST['nano_adsense_size'];
         $ad_str_time = $_POST['nano_ad_str_date'];
         $ad_end_time = $_POST['nano_ad_end_date'];
         $ad_status = ( isset( $_POST['nano_ad_active'] ) ? '1' : '0' );
@@ -107,9 +94,6 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
                     'ad_code'           => $ad_code,
                     'ad_image_url'      => $ad_image_url,
                     'url'               => $ad_url,
-                    'adsense_pub_id'    => $adsense_pub_id,
-                    'adsense_ad_slot'   => $adsense_ad_id,
-                    'adsense_ad_size'   => $adsense_size,
                     'str_time'          => $ad_str_time,
                     'end_time'          => $ad_end_time,
                     'ad_status'         => $ad_status
@@ -120,15 +104,14 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
         // validation
         if( ( ( $ad_str_time && $ad_end_time ) != '' )
             && (
-                ($_POST['nano_ad_type'] == 'imagead' && ($ad_image_url && $ad_url) != '')
-                || ($_POST['nano_ad_type'] == 'googleadsense' && ($adsense_pub_id && $adsense_ad_id && $adsense_size) != '')
-                || ($_POST['nano_ad_type'] == 'codead' && $ad_code != '')
+                ($_POST['nano_ad_type'] === 'imagead' && ($ad_image_url && $ad_url) != '')
+                || ($_POST['nano_ad_type'] === 'codead' && $ad_code != '')
             ) )
             {
                 $success = $wpdb->update( $table, $data, $where );
 
                 // Redirect to view ad page
-                header( "location:" . admin_url('/admin.php?page=site-ad&edit=1&success=true') );
+                header( "location:" . admin_url('/admin.php?page=adzonia&edit=1&success') );
             }
 
         if(isset($success)) {
@@ -192,37 +175,26 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
             </div> <!-- #titlediv -->
 
             <div id="selectdiv" class="row mt10">
-                <strong style="font-size: 1.1em;"><?php _e( 'Ad Type:',PLUGINTEXTDOMAIN ); ?></strong>
+                <strong style="font-size: 1.1em;"><?php _e( 'Type of Ad:','ad-zonia' ); ?></strong>
                 <select name="nano_ad_type" id="nano_ad_type">
                     <option value="">Select One</option>
                     <option id="id1" value="imagead"
                         <?php
                         if ( isset($_POST['nano_ad_type']) && $_POST['nano_ad_type'] != '' ) {
-                            echo ( $_POST['nano_ad_type'] == 'imagead' ? 'selected="selected"' : 'disabled="disabled"' );
+                            echo ( $_POST['nano_ad_type'] === 'imagead' ? 'selected="selected"' : 'disabled="disabled"' );
                         } elseif( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->ad_type) ) {
-                            echo ( $ad_query_with_id[0]->ad_type == 'imagead' ? 'selected="selected"' : 'disabled="disabled"' );
+                            echo ( $ad_query_with_id[0]->ad_type === 'imagead' ? 'selected="selected"' : 'disabled="disabled"' );
                         } else {
                             echo '';
                         }
                         ?>
                     >Image Ad</option>
-                    <option id="id2" value="googleadsense"
-                        <?php
-                        if ( isset($_POST['nano_ad_type']) && $_POST['nano_ad_type'] != '' ) {
-                            echo ( $_POST['nano_ad_type'] == 'googleadsense' ? 'selected="selected"' : 'disabled="disabled"' );
-                        } elseif( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->ad_type) ) {
-                            echo ( $ad_query_with_id[0]->ad_type == 'googleadsense' ? 'selected="selected"' : 'disabled="disabled"' );
-                        } else {
-                            echo '';
-                        }
-                        ?>
-                        >Google AdSense</option>
                     <option id="id3" value="codead"
                         <?php
                         if ( isset($_POST['nano_ad_type']) && $_POST['nano_ad_type'] != '' ) {
-                            echo ( $_POST['nano_ad_type'] == 'codead' ? 'selected="selected"' : 'disabled="disabled"' );
+                            echo ( $_POST['nano_ad_type'] === 'codead' ? 'selected="selected"' : 'disabled="disabled"' );
                         } elseif( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->ad_type) ) {
-                            echo ( $ad_query_with_id[0]->ad_type == 'codead' ? 'selected="selected"' : 'disabled="disabled"' );
+                            echo ( $ad_query_with_id[0]->ad_type === 'codead' ? 'selected="selected"' : 'disabled="disabled"' );
                         } else {
                             echo '';
                         }
@@ -258,90 +230,6 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
                 }
                 ?>" id="nano-ad-url">
             </div> <!-- #imagediv -->
-
-            <div id="googleadsense" class="togglediv row mt10 form-elements hide-me">
-                <div class="nano-ad-block-left">
-                    <label class="ad-label" for="nano_adsense_pub_id">Publisher ID<span class="required">*</span></label>
-                    <p class="direction">Write down your AdSense Publisher/Ad Client ID</p>
-                    <input type="text" name="nano_adsense_pub_id" placeholder="i.e.: ca-pub-3526212672565487" size="30" value="<?php
-                    if( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->adsense_pub_id) ) {
-                        echo $ad_query_with_id[0]->adsense_pub_id;
-                    } elseif( isset($_POST['nano_adsense_pub_id']) && $_POST['nano_adsense_pub_id'] != '' ) {
-                        echo $_POST['nano_adsense_pub_id'];
-                    } else {
-                        echo '';
-                    }
-                    ?>" id="nano-adsense-pub-id">
-
-                    <label class="ad-label" for="nano_adsense_ad_id">Ad Slot ID<span class="required">*</span></label>
-                    <p class="direction">Write down your Google AdSense Ad Slot ID</p>
-                    <input type="text" name="nano_adsense_ad_id" placeholder="i.e.: 8657754785" size="30" value="<?php
-                    if( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->adsense_ad_slot) ) {
-                        echo $ad_query_with_id[0]->adsense_ad_slot;
-                    } elseif( isset($_POST['nano_adsense_ad_id']) && $_POST['nano_adsense_ad_id'] != '' ) {
-                        echo $_POST['nano_adsense_ad_id'];
-                    } else {
-                        echo '';
-                    }
-                    ?>" id="nano-adsense-ad-id">
-                </div> <!-- .nano-ad-block-left -->
-
-                <div class="nano-ad-block-right">
-                    <label class="ad-label" for="nano_adsense_size">Width<span class="required">*</span></label>
-                    <p class="direction">Choose the ad size from the dropdown below</p>
-                    <?php
-                    // Google AdSense Ad Sizes
-                    $ad_sizes = array(
-                        '728x90'    => 'Leaderboard (728 &times; 90px)',
-                        '468x90'    => 'Banner (468 &times; 90px)',
-                        '234x60'    => 'Half Banner (234 &times; 60px)',
-                        '125x125'   => 'Button (125 &times; 125px)',
-                        '120x600'   => 'Skycrapper (120 &times; 600px)',
-                        '160x600'   => 'Wide Skycrapper (160 &times; 600px)',
-                        '180x150'   => 'Small Rectangle (180 &times; 150px)',
-                        '120x240'   => 'Vertical Banner (120 &times; 240px)',
-                        '200x200'   => 'Small Square (200 &times; 200px)',
-                        '250x250'   => 'Square (250 &times; 250px)',
-                        '300x250'   => 'Medium Rectangle (300 &times; 250px)',
-                        '336x280'   => 'Large Rectangle (336 &times; 280px)',
-                        '320x50'    => 'Mobile Leaderboard (320 &times; 50px)',
-                        '320x100'   => 'Large Mobile Banner (320 &times; 100px)',
-                        '468x60'    => 'Banner (Mobile) (468 &times; 60px)',
-                        '300x600'   => 'Large Skycrapper&frasl; Half Page (300 &times; 600px)',
-                        '970x90'    => 'Large Leaderboard (970 &times; 90px)',
-                        '240x400'   => 'Vertical Rectangle* (240 &times; 400px)',
-                        '980x120'   => 'Panorama* (980 &times; 120px)',
-                        '250x360'   => 'Triple Widescreen* (250 &times; 360px)',
-                        '930x180'   => 'Top Banner* (930 &times; 180px)',
-                        '580x400'   => 'Netboard* (580 &times; 400px)',
-                        '750x300'   => 'Triple Billboard* (750 &times; 300px)',
-                        '750x200'   => 'Double Billboard* (750 &times; 200px)',
-                        '750x100'   => 'Billboard* (750 &times; 100px)'
-                    );
-
-                    ?>
-                    <select name="nano_adsense_size" id="nano-adsense-size">
-                        <option value="">Choose one</option>
-                        <?php foreach( $ad_sizes as $size => $adsize ) { ?>
-                            <option value="<?php echo $size; ?>"
-                                <?php
-                                if( isset( $ad_query_with_id ) && !empty($ad_query_with_id[0]->adsense_ad_size) ) {
-                                    if( $ad_query_with_id[0]->adsense_ad_size == $size )
-                                        echo 'selected="selected"';
-                                } elseif( isset($_POST['nano_adsense_size']) && $_POST['nano_adsense_size'] != '' ) {
-                                    if( $_POST['nano_adsense_size'] == $size )
-                                        echo 'selected="selected"';
-                                } else {
-                                    echo '';
-                                }
-                                ?>
-                            ><?php echo $adsize; ?></option>
-                        <?php } //endforeach ?>
-                    </select>
-                    <br/><br/>
-                    * - regional ad sizes by Google AdSense
-                </div> <!-- .nano-ad-block-right -->
-            </div> <!-- #googleadsense -->
 
             <div id="codead" class="togglediv codediv row mt10 form-elements hide-me">
 
@@ -433,12 +321,12 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
                 <div class="nano-ad-submit-zone">
 
                     <div class="row">
-                        <p><?php _e('After filling the necessary fields, to show the advertisement on the site, remember to <u>make the ad <strong>Active</strong></u>, and then Save the data.',  PLUGINTEXTDOMAIN); ?></p>
+                        <p><?php _e('After filling the necessary fields, to show the advertisement on the site, remember to <u>make the ad <strong>Active</strong></u>, and then Save the data.',  'ad-zonia'); ?></p>
                     </div>
 
                     <div class="nano-ad-submit row">
                         <label><input type="checkbox" name="nano_ad_active" id="nano_ad_active" value="1" <?php
-                            if( isset( $ad_query_with_id ) && $ad_query_with_id[0]->ad_status == 1 ) {
+                            if( isset( $ad_query_with_id ) && $ad_query_with_id[0]->ad_status === '1' ) {
                                 echo 'checked="checked"';
                             } elseif( isset($_POST['nano_ad_active']) && $_POST['nano_ad_active'] != '' ) {
                                 echo 'checked="checked"';
@@ -454,7 +342,7 @@ if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) {
                                         echo 'Make Active';
                                     }
                                     ?></strong></span></label>
-                        <?php if( isset( $_GET["action"] ) && $_GET["action"] == 'edit' ) { ?>
+                        <?php if( isset( $_GET["action"] ) && $_GET["action"] === 'edit' ) { ?>
                             <input type="submit" name="nano_ad_update" id="nano-ad-publish" class="button button-primary button-large" value="Update">
                         <?php } else { ?>
                             <input type="submit" name="nano_ad_publish" id="nano-ad-publish" class="button button-primary button-large" value="Publish">
